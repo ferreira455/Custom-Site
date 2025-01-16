50359aa8a489cce1ff2ea54e4c554fe7b41a9da2
Gabriel <gabrielgamer7793@gmail.com>
2025-01-16 16:20:26 -0300

    Mensagem do commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
@@ -17,6 +17,18 @@ videos = {
 def homepage():
     # Seleciona um vídeo aleatório
     random_video = random.choice(list(videos.values()))
+    
+    # Obtém a URL onde a aplicação está rodando
+    app_url = request.url_root
+    
+    # Cria a URL completa do arquivo de vídeo
+    video_url = app_url + random_video
+
+    # Exibe as URLs
+    print(f"A aplicação está rodando em: {app_url}")
+    print(f"A URL do vídeo 'alone.mp4' é: {app_url}static/videos/alone.mp4")
+
+    # Retorna o template com o vídeo aleatório
     return render_template('index.html', background=random_video)
 
 if __name__ == '__main__':
